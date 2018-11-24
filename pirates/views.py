39d53dc9f5c5017318c5eb996e98d4e42bed6118
analuizatrz <analuizatrz@gmail.com>
2018-11-24 14:58:21 -0200

    begin view

--- pirates/views.py
+++ pirates/views.py
@@ -1,2 +1,7 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Tesouro
+
+class ListaTesourosView(View):
+	def get(self, request):
+		return render(request, 'lista_tesouros.html', None)
--- web_pirates/urls.py
+++ web_pirates/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from pirates.views import ListaTesourosView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ListaTesourosView.as_view()),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
